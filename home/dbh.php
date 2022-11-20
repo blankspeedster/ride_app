bc530b7166ca07e784bba5589017593c65739182
bituinronie <ronie.bituin@trainocate.com>
2022-11-20 13:22:32 +1100

    Create dbh.php

---
+++ home/dbh.php
@@ -0,0 +1,15 @@
+<?php
+
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+
+    $host = 'localhost';
+    $username = 'ride_app_database';
+    $password = 'ride_app_database';
+    $database = 'ride_app_database';
+
+    $mysqli = new mysqli($host,$username,$password,$database) or die(mysqli_error($mysqli));
+
+?>
